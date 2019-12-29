d4db628931c0b175a17299986d8fe00e1e321246
Gabriel Ferreira <gabriel.ferreira@iclinic.com.br>
2019-12-29 20:22:08 -0300

    ci(prettier): add

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,28 @@
-import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React from "react"
+import { Route, Switch, Redirect } from "react-router-dom"
+import { useSelector } from "react-redux"
 
-import Login from './containers/Login';
-import Entry from './containers/Entry';
-import Code from './containers/Code';
+import Login from "./containers/Login"
+import Entry from "./containers/Entry"
+import Code from "./containers/Code"
 
 const PrivateRoutes = () => (
-    <>
-        <Route path="/entry" component={Entry}/>
-        <Route path="/code" component={Code} />
-    </>
-);
+  <>
+    <Route path="/entry" component={Entry} />
+    <Route path="/code" component={Code} />
+  </>
+)
 
 const Routes = () => {
-    const user = useSelector(({ user: { data } }) => data);
+  const user = useSelector(({ user: { data } }) => data)
 
-    return (
-        <Switch>
-            <Route path="/" exact component={Login}/>
-            <Route path="/login" component={Login}/>
-            {user ? <PrivateRoutes /> : <Redirect to="/" />}
-        </Switch>
-    )
-};
+  return (
+    <Switch>
+      <Route path="/" exact component={Login} />
+      <Route path="/login" component={Login} />
+      {user ? <PrivateRoutes /> : <Redirect to="/" />}
+    </Switch>
+  )
+}
 
-export default Routes;
+export default Routes
