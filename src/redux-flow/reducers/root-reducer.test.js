0dc9defc7f870bb29c76927b39a22b23da314018
Gabriel Ferreira <gabrielferreira.95@hotmail.com>
2020-01-02 16:28:06 -0300

    test(root-reducer): refactor (#10)

--- src/redux-flow/reducers/root-reducer.test.js
+++ src/redux-flow/reducers/root-reducer.test.js
@@ -7,11 +7,14 @@ import user from "./user"
 import rootReducer from "./root-reducer"
 
 describe("reducers/root-reducer", () => {
+  let store
+  beforeEach(() => {
+    const history = createMemoryHistory()
+    store = createStore(rootReducer(history))
+  })
+
   describe("reducers/auth", () => {
     test("should return initial state", () => {
-      const history = createMemoryHistory()
-      const store = createStore(rootReducer(history))
-
       expect(store.getState().auth).to.be.deep.equal(
         auth(undefined, {
           type: "unknown",
@@ -20,9 +23,6 @@ describe("reducers/root-reducer", () => {
     })
 
     test("should dispatch action `auth/REQUEST`", () => {
-      const history = createMemoryHistory()
-      const store = createStore(rootReducer(history))
-
       const action = {
         type: "auth/REQUEST",
         payload: {
@@ -35,9 +35,6 @@ describe("reducers/root-reducer", () => {
     })
 
     test("should dispatch action `auth/SUCCESS`", () => {
-      const history = createMemoryHistory()
-      const store = createStore(rootReducer(history))
-
       const action = {
         type: "auth/SUCCESS",
         payload: {
@@ -51,9 +48,6 @@ describe("reducers/root-reducer", () => {
     })
 
     test("should dispatch action `auth/FAILURE`", () => {
-      const history = createMemoryHistory()
-      const store = createStore(rootReducer(history))
-
       const action = {
         type: "auth/FAILURE",
         payload: {
@@ -68,9 +62,6 @@ describe("reducers/root-reducer", () => {
 
   describe("reducers/user", () => {
     test("should return initial state", () => {
-      const history = createMemoryHistory()
-      const store = createStore(rootReducer(history))
-
       expect(store.getState().user).to.be.deep.equal(
         user(undefined, {
           type: "unknown",
@@ -79,9 +70,6 @@ describe("reducers/root-reducer", () => {
     })
 
     test("should dispatch action `user/SYNC_REQUEST`", () => {
-      const history = createMemoryHistory()
-      const store = createStore(rootReducer(history))
-
       const action = {
         type: "user/SYNC_REQUEST",
         payload: {
@@ -94,9 +82,6 @@ describe("reducers/root-reducer", () => {
     })
 
     test("should dispatch action `user/SYNC_SUCCESS`", () => {
-      const history = createMemoryHistory()
-      const store = createStore(rootReducer(history))
-
       const action = {
         type: "user/SYNC_SUCCESS",
         payload: {
@@ -109,9 +94,6 @@ describe("reducers/root-reducer", () => {
     })
 
     test("should dispatch action `user/SYNC_FAILURE`", () => {
-      const history = createMemoryHistory()
-      const store = createStore(rootReducer(history))
-
       const action = {
         type: "user/SYNC_FAILURE",
         payload: {
