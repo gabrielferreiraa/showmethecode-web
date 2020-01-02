e8edc2e45d63f1ffb03cb23dea58f7d5bde133f7
Gabriel Ferreira <gabriel.ferreira@iclinic.com.br>
2020-01-02 17:08:13 -0300

    test(root-reducer): refactor

--- src/redux-flow/reducers/root-reducer.test.js
+++ src/redux-flow/reducers/root-reducer.test.js
@@ -23,40 +23,46 @@ describe("reducers/root-reducer", () => {
     })
 
     test("should dispatch action `auth/REQUEST`", () => {
-      const action = {
+      const authRequestAction = {
         type: "auth/REQUEST",
         payload: {
           isFetching: true,
         },
       }
-      store.dispatch(action)
+      store.dispatch(authRequestAction)
 
-      expect(store.getState().auth).to.be.deep.equal(auth(undefined, action))
+      expect(store.getState().auth).to.be.deep.equal(
+        auth(undefined, authRequestAction)
+      )
     })
 
     test("should dispatch action `auth/SUCCESS`", () => {
-      const action = {
+      const authSuccessAction = {
         type: "auth/SUCCESS",
         payload: {
           isFetching: false,
           token: "TOKEN",
         },
       }
-      store.dispatch(action)
+      store.dispatch(authSuccessAction)
 
-      expect(store.getState().auth).to.be.deep.equal(auth(undefined, action))
+      expect(store.getState().auth).to.be.deep.equal(
+        auth(undefined, authSuccessAction)
+      )
     })
 
     test("should dispatch action `auth/FAILURE`", () => {
-      const action = {
+      const authFailureAction = {
         type: "auth/FAILURE",
         payload: {
           isFetching: false,
         },
       }
-      store.dispatch(action)
+      store.dispatch(authFailureAction)
 
-      expect(store.getState().auth).to.be.deep.equal(auth(undefined, action))
+      expect(store.getState().auth).to.be.deep.equal(
+        auth(undefined, authFailureAction)
+      )
     })
   })
 
@@ -70,39 +76,45 @@ describe("reducers/root-reducer", () => {
     })
 
     test("should dispatch action `user/SYNC_REQUEST`", () => {
-      const action = {
+      const syncRequestAction = {
         type: "user/SYNC_REQUEST",
         payload: {
           isFetching: true,
         },
       }
-      store.dispatch(action)
+      store.dispatch(syncRequestAction)
 
-      expect(store.getState().user).to.be.deep.equal(user(undefined, action))
+      expect(store.getState().user).to.be.deep.equal(
+        user(undefined, syncRequestAction)
+      )
     })
 
     test("should dispatch action `user/SYNC_SUCCESS`", () => {
-      const action = {
+      const syncSuccessAction = {
         type: "user/SYNC_SUCCESS",
         payload: {
           isFetching: false,
         },
       }
-      store.dispatch(action)
+      store.dispatch(syncSuccessAction)
 
-      expect(store.getState().user).to.be.deep.equal(user(undefined, action))
+      expect(store.getState().user).to.be.deep.equal(
+        user(undefined, syncSuccessAction)
+      )
     })
 
     test("should dispatch action `user/SYNC_FAILURE`", () => {
-      const action = {
+      const syncFailureAction = {
         type: "user/SYNC_FAILURE",
         payload: {
           isFetching: false,
         },
       }
-      store.dispatch(action)
+      store.dispatch(syncFailureAction)
 
-      expect(store.getState().user).to.be.deep.equal(user(undefined, action))
+      expect(store.getState().user).to.be.deep.equal(
+        user(undefined, syncFailureAction)
+      )
     })
   })
 })
