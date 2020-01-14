0e0d203f3fe00f93d6c2655d3ad1c29fb28f8a1d
Gabriel Ferreira <gabriel.ferreira@iclinic.com.br>
2020-01-13 23:47:11 -0300

    style(login): add logo n text

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -7,18 +7,17 @@ import { Text, Icon } from "components"
 import Github from "./Github"
 import Google from "./Google"
 import Wrapper from "./Wrapper"
+import Logo from "./Logo"
 
 export default function Login() {
   const dispatch = useDispatch()
 
   return (
     <div>
+      <Logo src="images/show-me-the-code.png" alt="" title="" />
       <Text align="center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus
-        orci purus. Aenean ut turpis urna. Proin at sapien quis nibh gravida
-        rutrum. Integer aliquet leo imperdiet, venenatis nibh et, laoreet magna.
-        Praesent justo velit, vulputate ut diam sit amet, volutpat feugiat
-        sapien. Proin eget mollis arcu. Curabitur fermentum finibus ultrices.
+        Real-time code sharing with private rooms to make your life and your
+        friends easier. Talking is cheap, show me the code!
       </Text>
       <Wrapper>
         <Github onClick={() => dispatch(loginRequest(PROVIDERS.github))}>
