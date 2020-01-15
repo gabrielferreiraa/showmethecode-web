3add21284b5c4483cdc5717427d4bbf35a22ee74
Gabriel Ferreira <gabrielferreira.95@hotmail.com>
2020-01-15 00:40:20 -0300

    test(add render tests to login component): entry (#22)

--- src/containers/Login/Logo.js
+++ src/containers/Login/Logo.js
@@ -1,7 +1,19 @@
+import React from "react"
 import styled from "styled-components"
+import global from "config/global"
 
-export default styled.img`
+const Image = styled.img`
   width: 600px;
   display: block;
   margin: 0 auto;
 `
+
+export default function Logo() {
+  return (
+    <Image
+      src="images/show-me-the-code.png"
+      alt={global.appName}
+      title={global.appName}
+    />
+  )
+}
